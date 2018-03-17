8166e969d257811ce4ed6fb381956f7274009084
Bartosz Wojtaś <el.klides@op.pl>
2018-03-17 12:05:14 +0100

    KK

---
+++ components/App.js
@@ -0,0 +1,93 @@
+App = React.createClass({
+  getInitialState() {
+    return {
+      loading: false,
+      searchingText: '',
+      gif: {}
+    };
+  },
+	
+  handleSearch: function(searchingText) {
+    this.setState({
+      loading: true
+    });
+    this.getGif(searchingText, function(gif) {
+      this.setState({
+        loading: false,
+        gif: gif,
+        searchingText: searchingText
+      });
+    }.bind(this));
+  },
+  
+  /*getGif: function(searchingText, callback) {
+    var url = 'https://api.giphy.com' + '/v1/gifs/random?api_key=' + '79d4ejI5WnmHsZqbA6TZOyeG2iB4101D' + '&tag=' + searchingText;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText).data;
+        var gif = {
+          url: data.fixed_width_downsampled_url,
+          sourceUrl: data.url
+        };
+        callback(gif);
+      }
+    };
+    xhr.send();
+  },*/
+  
+  function getGif(url) {
+    return new Promise(
+        function (resolve, reject) {
+            const request = new XMLHttpRequest();
+            request.onload = function () {
+                if (this.status === 200) {
+                    resolve(this.response); // Sukces
+                } else {
+                    reject(new Error(this.statusText)); // Dostaliśmy odpowiedź, ale jest to np 404
+                }
+            };
+            request.onerror = function () {
+                reject(new Error(
+                   `XMLHttpRequest Error: ${this.statusText}`));
+            };
+            request.open('GET', url);
+            request.send();
+        }
+	);
+  }
+  
+  getGif('https://api.giphy.com' + '/v1/gifs/random?api_key=' + '79d4ejI5WnmHsZqbA6TZOyeG2iB4101D' + '&tag=' + searchingText)
+.then(response => {
+        var data = JSON.parse(xhr.responseText).data;
+        var gif = {
+          url: data.fixed_width_downsampled_url,
+          sourceUrl: data.url
+        };
+        callback(gif);
+      })
+.catch(error => console.error('Something went wrong', error));
+	
+  render: function() {
+
+    var styles = {
+      margin: '0 auto',
+      textAlign: 'center',
+      width: '90%'
+    };
+
+    return (
+      <div style={styles}>
+        <h1>Wyszukiwarka GIFow!</h1>
+        <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+        <Search onSearch={this.handleSearch}/>
+        <Gif 
+          loading={this.state.loading}
+          url={this.state.gif.url}
+          sourceUrl={this.state.gif.sourceUrl}
+        />
+      </div>
+    );
+  }
+});
